c25593e13915ea699c4ab3f1074a66e33080be1d
DimonSamogon77 <zyzychka1@gmail.com>
2021-04-14 21:46:39 +0300

    a

--- src/main/java/com/dimon/cars/dao/CarDAO.java
+++ src/main/java/com/dimon/cars/dao/CarDAO.java
@@ -1,49 +1,55 @@
 package com.dimon.cars.dao;
 
 import com.dimon.cars.models.Car;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 @Component
 public class CarDAO {
-    private static int CARS_COUNT;
-    private final List<Car> cars;
+    private final JdbcTemplate jdbcTemplate;
 
-    {
-        cars = new ArrayList<>();
-        cars.add(new Car(++CARS_COUNT, 2021, "Volkswagen", "Tiguan", "1.4", "150", "250", "6AMT"));
-        cars.add(new Car(++CARS_COUNT, 2012,"Renault", "Fluence", "1.6", "110", "145", "4AT"));
-        cars.add(new Car(++CARS_COUNT, 2011, "Renault", "Logan", "1.6", "84", "124", "6MT"));
+    @Autowired
+    public CarDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-    public List<Car> index(){
-        return cars;
+    public List<Car> index() {
+        return jdbcTemplate.query("select * from car", new BeanPropertyRowMapper<>(Car.class));
     }
 
-    public void save(Car car){
-        car.setId(++CARS_COUNT);
-        cars.add(car);
+
+    public void save(Car car) {
+        jdbcTemplate.update("insert into car (brand, model, engineCapacity, hp, torque, transmission, year) values(?,?,?,?,?,?,?)", car.getBrand(), car.getModel(),
+                car.getEngineCapacity(), car.getHp(), car.getTorque(), car.getTransmission(), car.getYear());
     }
 
-    public Car show(int id){
-        return cars.get(id-1);
+
+    public Car show(int id) {
+        return jdbcTemplate.queryForObject("select * from car where id = ?", new Object[]{id}, (resultSet, i) -> {
+            Car car = new Car();
+            car.setBrand(resultSet.getString("brand"));
+            car.setModel(resultSet.getString("model"));
+            car.setEngineCapacity(resultSet.getString("engineCapacity"));
+            car.setHp(resultSet.getString("hp"));
+            car.setTorque(resultSet.getString("torque"));
+            car.setTransmission(resultSet.getString("transmission"));
+            car.setYear(resultSet.getInt("year"));
+            return car;
+        });
     }
 
-    public void update(int id, Car car){
-        Car carToBeUpdated = show(id);
-        carToBeUpdated.setBrand(car.getBrand());
-        carToBeUpdated.setModel(car.getModel());
-        carToBeUpdated.setEngineCapacity(car.getEngineCapacity());
-        carToBeUpdated.setHp(car.getHp());
-        carToBeUpdated.setTorque(car.getTorque());
-        carToBeUpdated.setTransmission(car.getTransmission());
-        carToBeUpdated.setYear(car.getYear());
+    public void update(int id, Car car) {
+        jdbcTemplate.update("update car set brand=?, model=?, engineCapacity=?, hp=?, torque=?, transmission=?, year=? where id=?", car.getBrand(), car.getModel(),
+                car.getEngineCapacity(), car.getHp(), car.getTorque(), car.getTransmission(), car.getYear(), id);
     }
 
-    public void delete(int id){
-        cars.remove(id-1);
+    public void delete(int id) {
+        jdbcTemplate.update("delete from car where id=?", id);
     }
-
 }
